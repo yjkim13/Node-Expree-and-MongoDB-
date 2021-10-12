8e7f89057e131a02963d1f7b955aed5df0e92464
yjkim13 <sk940913@naver.com>
2021-10-13 00:55:14 +0900

    MongoDB 로그인 오류 수정

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const url = 'mongodb+srv://kim:1q2w3e4r@cluster0.brcl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const mongoose = require('mongoose')
-mongoose.connect('');
+
+
+const mongoose = require('mongoose');
+mongoose.connect(url)
+    .then(()=> console.log('MoggoDBConnected..'))
+    .catch((err) => console.log(err));
 
 app.get('/',(req,res) => res.send('Hello World'));
 
