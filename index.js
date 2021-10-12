a2938dc1125895e1371f89f0cc5db1cbfe82a423
yjkim13 <sk940913@naver.com>
2021-10-12 23:53:01 +0900

    mongoDB 연결

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
+const mongoose = require('mongoose')
+mongoose.connect('');
+
 app.get('/',(req,res) => res.send('Hello World'));
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
