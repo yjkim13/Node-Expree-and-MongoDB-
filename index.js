57f859f94690fb6d9cf162c1b59687fdd7320044
yjkim13 <sk940913@naver.com>
2021-10-13 17:27:13 +0900

    비밀정보 보호 기능

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const express = require("express");
 const app = express();
 const port = 5000;
-const url = 'mongodb+srv://kim:1q2w3e4r@cluster0.brcl8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const bodyParser = require("body-parser");
+const { User} = require("./models/user");
+const config = require("./config/key");
 
+app.use(bodyParser.urlencoded({extended: true}));
 
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect(url)
+mongoose.connect(config.mongoURI)
     .then(()=> console.log('MoggoDBConnected..'))
     .catch((err) => console.log(err));
 
 app.get('/',(req,res) => res.send('Hello World'));
+
+app.post('/register',(req,res)=>{
+    //회원가입할 때 필요한 정보들을  clien에서 가져오면
+    //그것들을 데이터 베이스에 넣어준다.
+
+    const user = new User(req.body);
+    user.save((err,userInfo)=>{
+        if(err) return res.json({ success: false, err})
+        return res.status(200).json({
+            success: true
+        });
+    });
+});
 
 app.listen(port, ()=> console.log(`Example app listening on port ${port}!`));
